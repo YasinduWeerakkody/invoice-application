e60a22e747f58bdb2a87e65cb185ad18f5e81a3e
YasinduWeerakkody <127224509+YasinduWeerakkody@users.noreply.github.com>
2023-09-14 12:37:12 +0530

    update

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import invoiceIndex from '../components/invoices/index.vue'
 
+import invoiceNew from '../components/invoices/new.vue'
+
 import notfound from '../components/NotFound.vue'
 
 const routes = [
@@ -9,6 +11,12 @@ const routes = [
         path:'/',
         component: invoiceIndex
     },
+
+    {
+        path:'/invoice/new',
+        component: invoiceNew
+    },
+
     {
         path:'/:pathMatch(.*)*',
         component: notfound
